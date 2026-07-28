21f8938a37f4c683843d263484d90b125c10950a
zahra alipour <z.alipour7034891@gmail.com>
2026-07-28 12:43:03 +0430

    new initi

---
+++ src/datasets/brats.py
@@ -0,0 +1,16 @@
+from .base_dataset import BaseMedicalDataset
+
+class BraTSDataset(BaseMedicalDataset):
+
+    def __init__(self, root_dir, transform=None):
+        super().__init__(root_dir, transform)
+        self.samples = self.load_data()
+
+    def load_data(self):
+        return []
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        pass
